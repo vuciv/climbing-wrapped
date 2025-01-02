f3fa8172b20bd49837354b29ff107080b09e77c3
Joshua Fonseca <joshuafonseca@Joshuas-MBP.attlocal.net>
2025-01-01 19:06:10 -0600

    moved

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  assetsInclude: ['**/*.csv']  // Add this line to handle CSV files
+  assetsInclude: ['**/*.csv'],
+  base: './'
 })
